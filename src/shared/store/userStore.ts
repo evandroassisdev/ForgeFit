0f637ba44136963198f4650e3f4582cceeee9e29
Evandro Guilherme de Assis <evandroassisdev@Evandros-MacBook-Pro.local>
2026-05-11 10:34:41 -0300

    feat: zustand

---
+++ src/shared/store/userStore.ts
@@ -0,0 +1,41 @@
+import { UserInterface } from "../modules/user/user.types";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+interface SetSessionParams {
+  user: UserInterface;
+  token: string;
+  refreshToken: string;
+}
+
+interface UpdateTokensParams {
+  token: string;
+  refreshToken: string;
+}
+
+export interface UserStore {
+  user: UserInterface | null;
+  token: string | null;
+  refreshToken: string | null;
+
+  setSession: (sessionData: SetSessionParams) => void;
+  logout: () => void;
+  updateTokens: (updateTokensData: UpdateTokensParams) => void;
+}
+
+export const useUserStore = create<UserStore>()(persist(
+  (set) => ({
+    user: null,
+    token: null,
+    refreshToken: null,
+    logout: () => { },
+    setSession: (sessionData) => { set({ ...sessionData }) },
+    updateTokens: (updateTokensData: UpdateTokensParams) => { set({ ...updateTokensData }) }
+  }),
+  {
+    name: 'forgefit-storage',
+    storage: createJSONStorage(() => AsyncStorage),
+  }
+));
